6c09bb7039aa6fe8aeb132e45f219eb402c8373e
dhonaobina <dhona.obina@outlook.com>
2024-02-24 09:45:23 +1300

    add functions calculate, clearBox and displayInput, also modified button of operation with onclick function to displayInput()

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var val1;
+var val2;
+var resultval;
+var op;
+
 var color = "#ffff80";
 
 function setColor(id)
@@ -18,3 +23,48 @@ function displayValue(inputid)
     var element = document.getElementById(inputid);
     document.getElementById("txtinput").innerHTML = document.getElementById("txtinput").innerHTML + '' + element.value;
 }
+
+function displayInput(operation) 
+{
+    var1 = parseInt(document.getElementById("txtinput").innerHTML);
+    op = operation;
+    document.getElementById("txtinpt").innerHTML = op;
+    document.getElementById("txtinput").innerHTML = "";
+}
+
+function Calculate()
+{
+    val2 = parseInt(document.getElementById("txtinput").innerHTML);
+    document.getElementById("txtinput").innerHTML = "";
+    if (op == "*")
+    {
+        resultval = val1 * val2;
+    }
+    if (op == "+")
+    {
+        resultval = val1 + val2;
+    }
+    if (op == "-")
+    {
+        resultval = val1 - val2;
+    }
+    if (op == "/")
+    {
+        resultval = val1 / val2;
+    }
+    if (op == "%")
+    {
+        resultval = val1 % val2;
+    }
+    document.getElementById("txtinput").innerHTML = "";
+    document.getElementById("txtinput").innerHTML = resultval.toString();
+    document.getElementById("txtinput").innerHTML = val1 + op + val2 + " = " + resultval.toString();
+    return;
+}
+
+function clearBox()
+{
+    document.getElementById("txtinput").innerHTML = "";
+    return;
+}
+
